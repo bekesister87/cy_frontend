2f0946b5baac1a3039b71edd97b17ffb2770681a
bekesister@gmail.com <bekesister@gmail.com>
2022-02-07 18:54:09 +0100

    login page

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
+});
+
+fetch('https://schema.postman.com/json/collection/v2.1.0/collection.json', {
+  method: 'GET',
+})
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+
+
+
+
+
+
+
+
+
+
